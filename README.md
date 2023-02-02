d52cee40025489259782b75825d264002d5feae4
DOUGBOWMER <dougbowmer3@gmail.com>
2023-02-02 15:56:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nativescript-stackblitz-templates-jwseac
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nativescript-stackblitz-templates-jwseac)
---
+++ app/components/Home.svelte
@@ -0,0 +1,16 @@
+<page>
+    <actionBar title="Home" />
+    <gridLayout>
+        <label class="text-xl align-middle text-center text-white" text="{message}"/>
+    </gridLayout>
+</page>
+
+<script lang="ts">
+    let message: string = "Let Get Started!"
+</script>
+
+<style>
+    /* .info {
+        font-size: 20;
+    } */
+</style>
